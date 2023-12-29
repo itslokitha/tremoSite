9afcc03b298d0bc9d388d23927dab173ee44e936
itslokitha <anu99nilaweera@gmail.com>
2023-12-29 10:08:23 -0400

    Vercel speed insights added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,20 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+      </body>
+    </html>
+  );
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
       </body>
     </html>
   );
